b4d026153dc45c5275d4347b76e24f94d1a58b14
Mr. Tom <38649555+tmjns@users.noreply.github.com>
2021-03-19 18:30:41 +0100

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,24 @@
+import sys
+import subprocess 
+
+my_device = sys.argv[1]
+
+if __name__ == '__main__':
+    try:
+      pingpong = subprocess.Popen(["ping", my_device],stdout=subprocess.PIPE)
+      
+      while True:
+        line = pingpong.stdout.readline()
+        if not line:
+          break
+
+        connected_device = line.decode('utf-8').split()[3]
+        
+        if my_device in connected_device:
+          print(connected_device)
+          subprocess.Popen(["write", "connected to network"])
+          #execute your **** here
+    
+    except KeyboardInterrupt:
+        print(sys.stderr, '\nExiting by user request.\n')
+        sys.exit(0)
